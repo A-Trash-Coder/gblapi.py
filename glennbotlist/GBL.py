0ea01dcdd5195b958d282606b2a27024e5e2dfff
gavyn stanley <gavynstanley25@icloud.com>
2020-02-09 09:28:07 -0500

    Added some printing after a successful post. Added check to raise an error if not 200

--- glennbotlist/GBL.py
+++ glennbotlist/GBL.py
@@ -11,6 +11,7 @@ class GBL:
     def __init__(self, token: str, *, base: str = "https://glennbotlist.xyz/api/v2/bot/"):
         self.token = token
         self.base = base
+        self.session = aiohttp.ClientSession()
 
     async def post_server_count(self, botid: int, servers: int):
         """
@@ -21,4 +22,10 @@ class GBL:
         if not self.token:
             raise errors.InvalidKey("A token was not supplied")
 
-        await aiohttp.ClientSession().post(url=self.base + f"{botid}/stats", data={"serverCount": servers}, headers={"authorization": self.token})
+        async with self.session.post(url=self.base + f"{botid}/stats", data={"serverCount": servers}, headers={"authorization": self.token}) as r:
+            resp = await r.json()
+        
+        if resp['code'] != 200:
+            raise errors.InvalidKey("You're token was incorrect or another error has occured")
+        else:
+            print(f"Your server count of {servers} was posted successfully (Code: 200)")
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="gblpyapi",
-    version="0.1.0",
+    version="0.1.2",
     packages= ["glennbotlist"],
     url="https://glennbotlist.xyz",
     license="MIT",
