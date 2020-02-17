9551446565d7ddd2edf5ebcd6d9f41a7e5dd5b40
gavyn stanley <gavynstanley25@icloud.com>
2020-02-16 20:03:16 -0500

    I accidentally deleted setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+with open("README.md", "r") as fh:
+    long_desc = fh.read()
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setuptools.setup(
+    name="gblpyapi",
+    version="0.1.13",
+    packages= ["glennbotlist"],
+    url="https://glennbotlist.xyz",
+    license="MIT",
+    author="A Trash Coder",
+    author_email="gavynlamar@gmail.com",
+    description="glennbotlist.xyz API Wrapper in Python",
+    long_desciption=long_desc,
+    long_description_content_type="text/markdown",
+    requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent"
+    ]
+)
