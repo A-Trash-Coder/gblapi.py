f384d767e6c97a6181ea4372c5ae889ab98eef22
A-Trash-Coder <gavynlamar@gmail.com>
2020-04-29 00:17:29 -0400

    Fixed typo in setup.py. Bump version 2.0

--- setup.py
+++ setup.py
@@ -1,19 +1,19 @@
 import setuptools
-with open("README.md", "r") as fh:
+with open("README.md", "r", encoding="utf8") as fh:
     long_desc = fh.read()
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
 setuptools.setup(
     name="gblpyapi",
-    version="0.1.16",
+    version="0.2.0",
     packages= ["glennbotlist"],
     url="https://glennbotlist.xyz",
     license="MIT",
     author="A Trash Coder",
     author_email="gavynlamar@gmail.com",
     description="glennbotlist.xyz API Wrapper in Python",
-    long_desciption=long_desc,
+    long_description=long_desc,
     long_description_content_type="text/markdown",
     requires=requirements,
     classifiers=[
