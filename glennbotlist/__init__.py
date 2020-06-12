3e6403983e9a21aa8ee2f8152bcce4a47e6bbe80
A-Trash-Coder <gavynlamar@gmail.com>
2020-06-12 13:31:00 -0400

    Bump version

--- glennbotlist/__init__.py
+++ glennbotlist/__init__.py
@@ -7,10 +7,10 @@ __copyright__ = 'Copyright 2020 Gavyn Stanley'
 __author__ = "Gavyn Stanley (A Trash Coder)"
 __license__ = "MIT"
 __title__ = "gblapi.py"
-__version__ = "0.3.0"
+__version__ = "0.3.1"
 
 from collections import namedtuple
 
 VersionInfo = namedtuple('VersionInfo', 'major minor micro releaselevel serial')
 
-version_info = VersionInfo(major = 0, minor = 3, micro = 0, releaselevel = 'final', serial = 0)
+version_info = VersionInfo(major = 0, minor = 3, micro = 1, releaselevel = 'final', serial = 0)
